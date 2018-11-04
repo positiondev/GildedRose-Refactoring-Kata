41d2b856a9d4c926bbb89c38ac33a930951c1093
Libby Horacek <libby@daydrea.me>
2018-11-04 08:30:54 -0500

    Extract an ItemUpdater class

--- ruby/gilded_rose.rb
+++ ruby/gilded_rose.rb
@@ -4,68 +4,10 @@ class GildedRose
     @items = items
   end
 
-  def increase_quality(item)
-    if item.quality < 50
-     item.quality = item.quality + 1
-    end
-  end
-
-  def decrease_quality(item)
-    if item.quality > 0
-      if item.name != "Sulfuras, Hand of Ragnaros"
-        item.quality = item.quality - 1
-      end
-    end
-  end
-
-  def update_quality_of_brie(item)
-    increase_quality(item)
-
-    if item.sell_in < 0
-      increase_quality(item)
-    end
-  end
-
-  def update_quality_of_backstage(item)
-    increase_quality(item)
-
-    if item.sell_in < 11
-      increase_quality(item)
-    end
-    if item.sell_in < 6
-      increase_quality(item)
-    end
-    if item.sell_in < 0
-      item.quality = 0
-    end
-  end
-
-  def update_sell_in(item)
-    if item.name != "Sulfuras, Hand of Ragnaros"
-      item.sell_in = item.sell_in - 1
-    end
-  end
-
   def update_quality()
     @items.each do |item|
-      update_sell_in(item)
-
-      if item.name == "Aged Brie"
-        update_quality_of_brie(item)
-      end
-
-      if item.name == "Backstage passes to a TAFKAL80ETC concert"
-        update_quality_of_backstage(item)
-      end
-
-      if item.name !="Aged Brie" and
-         item.name != "Backstage passes to a TAFKAL80ETC concert"
-        decrease_quality(item)
-
-        if item.sell_in < 0
-          decrease_quality(item)
-        end
-      end
+      updater = ItemUpdater.new(item)
+      item = updater.update_item
     end
   end
 end
@@ -81,5 +23,86 @@ class Item
 
   def to_s()
     "#{@name}, #{@sell_in}, #{@quality}"
+  end
+end
+
+class ItemUpdater
+  attr_accessor :item, :name, :sell_in, :quality
+  def initialize(item)
+    @item = item
+    @name = item.name
+    @sell_in = item.sell_in
+    @quality = item.quality
+  end
+
+  def update_item
+    update_quality
+
+    item.quality = quality
+    item.sell_in = sell_in
+    item
+  end
+
+  def increase_quality
+    if quality < 50
+     self.quality = self.quality + 1
+    end
+  end
+
+  def decrease_quality
+    if quality > 0
+      if name != "Sulfuras, Hand of Ragnaros"
+        self.quality = self.quality - 1
+      end
+    end
+  end
+
+  def update_quality_of_brie
+    increase_quality
+
+    if sell_in < 0
+      increase_quality
+    end
+  end
+
+  def update_quality_of_backstage
+    increase_quality
+
+    if sell_in < 11
+      increase_quality
+    end
+    if sell_in < 6
+      increase_quality
+    end
+    if sell_in < 0
+      self.quality = 0
+    end
+  end
+
+  def update_sell_in
+    if name != "Sulfuras, Hand of Ragnaros"
+      self.sell_in = self.sell_in - 1
+    end
+  end
+
+  def update_quality
+    update_sell_in
+
+    if name == "Aged Brie"
+      update_quality_of_brie
+    end
+
+    if name == "Backstage passes to a TAFKAL80ETC concert"
+      update_quality_of_backstage
+    end
+
+    if name !="Aged Brie" and
+        name != "Backstage passes to a TAFKAL80ETC concert"
+      decrease_quality
+
+      if sell_in < 0
+        decrease_quality
+      end
+    end
   end
 end
